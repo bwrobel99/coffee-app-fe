d0ec479b2b2627533e9ba69c9f9846478d166ef0
g0gab1s <83614025+GabrielaGodek@users.noreply.github.com>
2023-05-24 21:30:53 +0200

    24.05
    Listing available coffees based on data from an API

--- src/components/ProductTile.vue
+++ src/components/ProductTile.vue
@@ -1,14 +1,30 @@
+<script setup>
+  import { defineProps } from 'vue';
+
+// get props from parent component
+  const props = defineProps({
+    coffee: {
+      type: Object,
+      required: true
+    }
+  });
+// Destructuring from props 
+  const { id, name, price, discount } = props.coffee;
+</script>
+
 <template>
-    <div class="product_tile">
-        <div class="image">
-            <img src="../assets/kawa_test.svg" alt="Americano">
-        </div>
-        <div class="desc">
-            <h1 class="title">Kawa</h1>
-            <div class="prices">
-                <div class="oldPrice">10</div>
-                <div class="newPrice">10</div>
-            </div>
-        </div>
+<!-- @click redirect to product page certain coffee with more details like description, size -->
+  <div class="tile" :id="id" @click="">
+    <div class="image">
+      <img src="../assets/kawa_test.svg" :alt="name">
     </div>
+    <div class="desc">
+      <h1 class="title">{{ name }}</h1>
+      <div class="prices">
+        <div class="oldPrice">{{ price }}</div>
+        <!-- if price after discount is lower than regular price display new price -->
+        <div class="newPrice" v-if="price > (price - discount)">{{ price - discount }}</div>
+      </div>
+    </div>
+  </div>
 </template>
