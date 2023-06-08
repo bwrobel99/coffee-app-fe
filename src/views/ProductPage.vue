ace50daed751cc219d31f9ef7b7d5625235e36fe
karolkart <karol.kowalski.ds@gmail.com>
2023-06-08 19:18:25 +0200

    Add ProductPage
    Dodanie karty z produktami i podpięcie jej do routera.

---
+++ src/views/ProductPage.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <ProductTile :coffee="selectedCoffee" />
+      <button @click="addToCart">Add to Cart</button>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  import ProductTile from '../components/ProductTile.vue';
+  
+  const selectedCoffee = ref(null); // Wybrane kawy
+  
+  // Metoda, która będzie wywoływana przy kliknięciu na konkretną kawę
+  const handleCoffeeClick = (coffee) => {
+    selectedCoffee.value = coffee;
+  };
+  
+  // Metoda, która będzie wywoływana przy kliknięciu na przycisk "Add to Cart"
+  const addToCart = () => {
+    if (selectedCoffee.value) {
+      // Dodaj kod obsługujący dodawanie do koszyka
+      console.log('Dodano do koszyka:', selectedCoffee.value);
+    }
+  };
+  </script>
