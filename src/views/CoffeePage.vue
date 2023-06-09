88d19a9874e841bd9f0f35352d36f0cce834cfa7
karol.kowalski <karol.kowalski@almine.pl>
2023-06-09 18:14:07 +0200

    Update CoffeePage

--- src/views/CoffeePage.vue
+++ src/views/CoffeePage.vue
@@ -1,36 +1,46 @@
-<script setup>
-  import ProductTile from '../components/ProductTile.vue';
 
+  <script setup>
+  import { ref, onMounted } from 'vue';
+  import { useRouter } from 'vue-router';
+  </script>
 
-
-</script>
-
-<script>
- export default {
-    data() {
-      return {
-        items: [] // Table to assign data from API
+  <script>
+  export default {
+    setup() {
+      const router = useRouter();
+      const coffee = ref(null);
+  
+      onMounted(() => {
+        const id = router.currentRoute.value.params.id;
+        fetch(`/api/coffees/:id`).then(res => {
+          if (res.ok) {
+            return res.json();
+          } else {
+            throw new Error('fetch error');
+          }
+        }).then(data => {
+          coffee.value = data;
+        });
+      });
+  
+      const addToCart = () => {
+        console.log("Added to cart", coffee.value);
       };
-    },
-    mounted() {
-      fetch("/api/coffees/:id").then(res => {
-        if(res.ok) {
-          return res.json()
-        } else {
-          throw new Error('fetch error');
-        }
-      }).then(data => {
-        // assign fetched data to table
-        this.items = data
-      })
+  
+      return {
+        coffee,
+        addToCart
+      };
     }
-  }
+  };
+  </script>
   
-</script>
-
-
-
-
-<template>
-  
-</template>
+  <template>
+    <main class="productpage">
+      <div class="productpage_wrapper">
+        <div class="product_tile">
+            {<ProductTile :coffee="items" />}
+          </div>
+        </div>
+    </main>
+  </template>
